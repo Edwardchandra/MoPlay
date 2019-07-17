94bef2efae999db3a9d42adcadf64b56f64d0543
Edward <edwardchandra14417@gmail.com>
2019-07-17 21:48:43 +0700

    Initial Commit

---
+++ MoPlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoPlay
+//
+//  Created by Edward Chandra on 17/07/19.
+//  Copyright © 2019 Edward Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
